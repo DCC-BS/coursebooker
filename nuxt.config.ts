7bd492448528987780806dbca1498d0abeca7440
Tobias Bollinger <tobias.bollinger@gmail.com>
2025-08-25 11:52:44 +0200

    init nuxt app

---
+++ nuxt.config.ts
@@ -0,0 +1,73 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    build: {
+        analyze: false,
+    },
+    runtimeConfig: {
+        githubToken: process.env.GITHUB_TOKEN,
+        apiUrl: process.env.API_URL,
+        public: {
+            logger_bs: {
+                loglevel: process.env.LOG_LEVEL || "debug",
+            },
+        },
+    },
+    // Define app head configuration
+    app: {
+        head: {
+            titleTemplate: 'CourseBooker',
+            htmlAttrs: {
+                lang: 'de',
+            },
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                {
+                    name: 'apple-mobile-web-app-title',
+                    content: 'CourseBooker',
+                },
+                { name: 'application-name', content: 'CourseBooker' },
+                { name: 'msapplication-config', content: '/browserconfig.xml' },
+            ],
+        },
+    },
+    ui: {
+        colorMode: false,
+    },
+    modules: [
+        '@nuxt/ui',
+        '@nuxtjs/i18n',
+        '@dcc-bs/common-ui.bs.js',
+        '@dcc-bs/logger.bs.js',
+        "@dcc-bs/feedback-control.bs.js",
+    ],
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+    "feedback-control.bs.js": {
+        repo: "Feedback",
+        owner: "DCC-BS",
+        project: "CourseBooker",
+        githubToken: process.env.GITHUB_TOKEN,
+    },
+    // localization
+    i18n: {
+        locales: [
+            {
+                code: "en",
+                name: "English",
+                file: "en.json",
+            },
+            {
+                code: "de",
+                name: "Deutsch",
+                file: "de.json",
+            },
+        ],
+        defaultLocale: "de",
+        strategy: "no_prefix",
+    },
+});
